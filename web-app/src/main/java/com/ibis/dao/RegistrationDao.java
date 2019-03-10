a8d3cb2a09b145441519616f51193c40d6b9a342
Elka165 <ela_dolna99.@o2.pl>
2019-03-10 11:19:36 +0100

    add property hibernate and class Registration

--- web-app/src/main/java/com/ibis/dao/RegistrationDao.java
+++ web-app/src/main/java/com/ibis/dao/RegistrationDao.java
@@ -1,4 +1,42 @@
 package com.ibis.dao;
 
+import com.ibis.model.Registration;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
 public class RegistrationDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public int save(Registration r){
+        entityManager.persist(r);
+        return r.getId();
+    }
+
+    public Registration update(Registration r){
+        return entityManager.merge(r);
+    }
+
+    public void delete (int id){
+        final Registration r=entityManager.find(Registration.class, id);
+        if(r!=null){
+            entityManager.remove(r);
+        }
+
+    }
+
+    public Registration findById (int id){
+        return entityManager.find(Registration.class, id);
+    }
+
+    public List<Registration> findAll(){
+        final Query query=entityManager.createQuery("Select r from Registration  r");
+        return query.getResultList();
+    }
 }
--- web-app/src/main/java/com/ibis/model/Registration.java
+++ web-app/src/main/java/com/ibis/model/Registration.java
@@ -1,7 +1,103 @@
 package com.ibis.model;
 
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "SM_UPRAWNIENIA_AKCEPT")
+
 public class Registration {
-    public void aa(){
-        System.out.println("ela");
+    private int id;
+    private String imie;
+    private String nazwisko;
+    private String nazwiskoImie;
+    private String login;
+    private LocalDate dataWyslania;
+    private String rodzajUprawnien;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "Id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Column(name = "Imie")
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+
+    @Column(name = "Nazwisko")
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+
+    @Column(name = "Nazwisko_Imie")
+    public String getNazwiskoImie() {
+        return nazwiskoImie;
+    }
+
+    public void setNazwiskoImie(String nazwiskoImie) {
+        this.nazwiskoImie = nazwiskoImie;
+    }
+
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+
+    @Column(name = "Data_wyslania")
+    public LocalDate getDataWyslania() {
+        return dataWyslania;
+    }
+
+    public void setDataWyslania(LocalDate dataWyslania) {
+        this.dataWyslania = dataWyslania;
+    }
+
+    @Column(name = "Rodzaj_Uprawnien")
+    public String getRodzajUprawnien() {
+        return rodzajUprawnien;
+    }
+
+    public void setRodzajUprawnien(String rodzajUprawnien) {
+        this.rodzajUprawnien = rodzajUprawnien;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration{" +
+                "id=" + id +
+                ", imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", nazwiskoImie='" + nazwiskoImie + '\'' +
+                ", login='" + login + '\'' +
+                ", dataWyslania=" + dataWyslania +
+                ", rodzajUprawnien='" + rodzajUprawnien + '\'' +
+                '}';
     }
 }
